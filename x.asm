831747f097056a1f6741fe820fb69f1c39a90169
cgangee <cgangee@gmail.com>
2013-06-26 04:05:09 +0800

    ==

--- x.asm
+++ x.asm
@@ -1,23 +1,53 @@
 .model small
 	.stack
 	.data
-	
+		msg byte "gao"
+		msglen word sizeof msg
 	.code
 start:
 	mov ax,@data
 	mov ds,ax
+
 	
-	call gao
+	mov cx, 5
+	mov bx, 0
+	g:
+		inc bx
+	loop g
+	call printNum
+	call gaoS
 	
 	mov ax,4c00h
 	int 21h
 		
-
+	gaoS proc
+		mov cx, msglen
+		mov bx, offset msg
+		gg:
+			push cx
+			call gao
+			pop cx
+			inc bx
+			
+		loop gg
+		ret
+	gaoS endp
+	
 	gao proc
 		mov ah, 2
-		mov dl, 'c'
+		mov dl, [bx]
+		
 		int 21h	
 		ret
 	gao endp
-	public start
-	end start
+	
+	printNum proc
+		mov ah, 2
+		add bl, '0'
+		mov dl, bl
+		int 21h
+		ret
+	printNum endp
+	
+public start
+end start
