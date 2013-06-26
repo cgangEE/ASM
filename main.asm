45bae634bd91f62e2fcb30a0e9047889c94e3158
cgangee <cgangee@gmail.com>
2013-06-26 15:52:31 +0800

    add main and proc to output dword in stack....

--- main.asm
+++ main.asm
@@ -5,7 +5,11 @@
 EXTERN printNum: far
 .code
 start:
+	mov eax, 132
+	push eax
 	call printNum 
+	add esp, 4
+	
 	mov eax, 4c00h
 	int 21h
 public start
--- proc.asm
+++ proc.asm
@@ -4,11 +4,37 @@
 .data
 PUBLIC printNum
 .code
+array byte 40 DUP(?)
+
 printNum PROC
-	mov ah, 2
-	mov dl, 'c'
-	int 21h
+	mov eax, [esp+4]
+	lea edi, array
+	whileloop: 
+		cmp eax, 0
+		jz endloop
+		mov edx, 0
+
+		mov ebx, 10
+		div ebx
+
+		mov [edi], dl
+		inc edi
+
+		jmp whileloop;
+	endloop:
+	
+	mov ecx, edi
+	sub ecx, offset array
+	
+	forloop:
+		dec edi
+		mov ah, 2
+		mov dl, [edi]
+		add dl, '0'
+		int 21h
+	loop forloop
+	
 	ret
 printNum ENDP
 
-end 
+end
